ea13f34aabaffb530b8b16127a1febe520336a27
liangchao.yan-ext <Liangchao.yan-ext@yanfeng.com>
2020-11-04 21:44:42 +0800

    create

---
+++ src/main/java/com/lcyan/demo/WebDemoApplication.java
@@ -0,0 +1,24 @@
+package com.lcyan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String demo(){
+		return "SUCCESS";
+	}
+}
+
+
+
+
